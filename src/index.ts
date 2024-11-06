13bdedd3168210b74d7fcd58906f11f4300f4b84
shaiksajidhussain <sanjusazid0@gmail.com>
2024-11-07 00:21:36 +0530

    Added the new url

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,15 @@ const port=9098
 const app=express()
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [
+    'https://qubicgen.com',
+    'http://qubicgen.com',
+    'http://localhost:5173',
+    'https://qg.vidyantra-dev.com'
+  ],
   credentials: true,
-  exposedHeaders: ['Access-Control-Allow-Origin']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use('/qubicgen',careerRouter)
 app.use('/qubicgen',getInTouchRouter)
